2d8e4fbb3dd06805060ca3c041fb4be70eeda67f
Lesenateur67 <aime.ackey@epita.fr>
2026-04-14 12:52:32 +0300

    Aime

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -156,7 +156,7 @@ public:
     // Example: "Student(Ali, ID: 101, GPA: 3.5)"
     friend ostream& operator<<(ostream& os, const Student& s) {
         // YOUR CODE HERE
-        os <<"Student("<<s.name", ID: "<<id<<", GPA: "<<gpa<<std::endl;
+        os <<"Student("<<s.name<<", ID: "<<id<<", GPA: "<<gpa<<std::endl;
         return os;
     }
 };
